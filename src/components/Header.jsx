bbd189705762311a59ddc61c8e91ea9c67872c72
UNIR-OscarCastro <oscar.castro928@comunidadunir.net>
2025-12-27 23:23:13 -0500

    Header Version Final
    Header version final con carrito desplegable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,63 @@
 import {Navbar, Container, Button} from "react-bootstrap";
 import IconCarrito from "./IconCarrito";
 import Nav from 'react-bootstrap/Nav';
-
+import { Link } from "react-router-dom";
+import logoHeader from "../assets/images/LibroHeader.png";
 
 
 const Header = () => {
     return (
        
-        <Navbar expand="lg" className="custom-navbar">
-            <Container>
-                 <Navbar.Brand href="/" className="d-flex align-items-center">
-                   
-                    <span className="fw-bold">Libreria Digital</span>
-                </Navbar.Brand>
-                 <Nav className="me-auto">
-                    <Nav.Link href="ListaLibros">Lista Libros</Nav.Link>
-                    <Nav.Link href="Carrito">Carrito</Nav.Link>
-                </Nav>
+     <header>
 
-                <IconCarrito/>
+      <Navbar variant="dark" expand="lg" role="navigation" className="custom-navbar-header">
+        <Container>
 
-            </Container>
-        </Navbar>
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            aria-label="Ir a la página principal de la librería digital"
+            className="d-flex align-items-center"
+          >
+            <img
+              src={logoHeader}
+              alt=""
+              aria-hidden="true"
+              width="110"
+              height="87"
+              className="me-2"
+            />
+            <span className="fw-bold">Librería Digital</span>
+          </Navbar.Brand>
+
+          <Navbar.Toggle
+            aria-controls="main-navbar"
+          />
+
+          <Navbar.Collapse id="main-navbar">
+            <Nav as="ul" className="me-auto">
+
+              <Nav.Item as="li">
+                <Nav.Link as={Link} to="/">
+                  | Inicio
+                </Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item as="li">
+                <Nav.Link as={Link} to="/ListaLibros">
+                  | Catálogo
+                </Nav.Link>
+              </Nav.Item>
+
+            </Nav>
+
+           <IconCarrito/>
+
+          </Navbar.Collapse>
+
+        </Container>
+      </Navbar>
+    </header>
     );
 };
 
